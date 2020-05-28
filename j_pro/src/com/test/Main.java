57322feb362b6312ab2f421b999d929ee259a850
god-obito <lbj12105@163.com>
2020-05-28 10:16:47 +0800

    4.修改之后再推送到远程仓库

--- j_pro/src/com/test/Main.java
+++ j_pro/src/com/test/Main.java
@@ -4,12 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("lalalala");
-        System.out.println("lalalala");
-        System.out.println("lalalala");
-        System.out.println("lalalala");
-        System.out.println("lalalala");
-        System.out.println("lalalala");
         System.out.println("222222222222");
         System.out.println("333333333333远程项目");
+
+        System.out.println("4.修改之后再推送到远程仓库");
+
     }
 }
